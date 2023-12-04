ac80aa4d58af8c650b2324d10e6819d76081a665
Fine974 <1370376731@qq.com>
2023-12-04 14:47:50 +0800

    Initial commit

---
+++ src/test/java/com/fine/bookmanagesystem/BookManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fine.bookmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
