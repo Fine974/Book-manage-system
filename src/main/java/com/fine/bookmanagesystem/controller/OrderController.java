d206f6a2812d33116f3d9717fc35cabb7fb859b2
zy <1824762582@qq.com>
2023-12-08 15:02:43 +0800

    下单功能的基本实现

--- src/main/java/com/fine/bookmanagesystem/controller/OrderController.java
+++ src/main/java/com/fine/bookmanagesystem/controller/OrderController.java
@@ -1,28 +1,47 @@
 package com.fine.bookmanagesystem.controller;
 
+import com.fine.bookmanagesystem.model.Cart;
 import com.fine.bookmanagesystem.model.Order;
-import com.fine.bookmanagesystem.model.Product;
+import com.fine.bookmanagesystem.service.CartService;
 import com.fine.bookmanagesystem.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final CartService cartService;
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, CartService cartService) {
         this.orderService = orderService;
+        this.cartService = cartService;
     }
 
     @PostMapping("/create")
-    public void createOrder(@RequestBody Order order) {
+    public String createOrder(@RequestBody Order order) {
         orderService.createOrder(order);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/placeOrder/{cartItemId}")
+    public String placeOrder(@PathVariable("cartItemId") int cartItemId) {
+        // 获取购物车项信息，然后调用服务层完成下单操作
+        Cart cartItem = cartService.getCartItemById(cartItemId); // 请根据实际情况替换为获取购物车项的方法
+        Order order = new Order();
+        order.setUserId(cartItem.getUserId());
+        order.setProductId(cartItem.getBookId());
+        order.setQuantity(cartItem.getQuantity());
+        order.setTotalPrice(cartItem.getBookPrice() * cartItem.getQuantity());
+        order.setOrderDate(new Date());
+        orderService.createOrder(order);
+        cartService.removeFromCart(cartItem.getId());
+        return "redirect:/orders/user/" + cartItem.getUserId();
     }
 
     @GetMapping("/user/{userId}")
